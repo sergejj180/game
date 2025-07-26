cd472b41fd4d5fcd36d441ab611a7aa703cdd457
sergejj180 <sergejj180@gmail.com>
2025-07-27 02:02:24 +0700

    date day 27 month 07 year 2025 time 02 hour 02 minute
    create class MonsterArcher

---
+++ MonsterArcher.cpp
@@ -0,0 +1,18 @@
+/*
+ * MonsterArcher.cpp
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "MonsterArcher.h"
+
+MonsterArcher::MonsterArcher() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MonsterArcher::~MonsterArcher() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ MonsterArcher.h
@@ -0,0 +1,19 @@
+/*
+ * MonsterArcher.h
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef MONSTERARCHER_H_
+#define MONSTERARCHER_H_
+
+#include "Archer.h"
+
+class MonsterArcher: public Archer {
+public:
+	MonsterArcher();
+	virtual ~MonsterArcher();
+};
+
+#endif /* MONSTERARCHER_H_ */
