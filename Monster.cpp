96ad96e65847108e36e60167b4926c7ab706f4d9
sergejj180 <sergejj180@gmail.com>
2025-07-27 02:16:02 +0700

    date day 27 month 07 year 2025 time 02 hour 15 minute
    create class Monster

---
+++ Monster.cpp
@@ -0,0 +1,18 @@
+/*
+ * Monster.cpp
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "Monster.h"
+
+Monster::Monster() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Monster::~Monster() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Monster.h
@@ -0,0 +1,17 @@
+/*
+ * Monster.h
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef MONSTER_H_
+#define MONSTER_H_
+
+class Monster {
+public:
+	Monster();
+	virtual ~Monster();
+};
+
+#endif /* MONSTER_H_ */
