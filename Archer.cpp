bd93c01c2e4a878be2b8ac43c658594a1559b8af
sergejj180 <sergejj180@gmail.com>
2025-07-27 01:46:02 +0700

    date day 27 month 07 year 2025 time 01 hour 45 minute
    create class Archer

---
+++ Archer.cpp
@@ -0,0 +1,18 @@
+/*
+ * Archer.cpp
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "Archer.h"
+
+Archer::Archer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Archer::~Archer() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Archer.h
@@ -0,0 +1,17 @@
+/*
+ * Archer.h
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef ARCHER_H_
+#define ARCHER_H_
+
+class Archer {
+public:
+	Archer();
+	virtual ~Archer();
+};
+
+#endif /* ARCHER_H_ */
