68cb056a8ab55abde0e34bad5713de658a08bbeb
sergejj180 <sergejj180@gmail.com>
2025-07-27 02:05:26 +0700

    date day 27 month 07 year 2025 time 02 hour 05 minute
    create class MonsterMage

---
+++ MonsterMage.cpp
@@ -0,0 +1,18 @@
+/*
+ * MonsterMage.cpp
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "MonsterMage.h"
+
+MonsterMage::MonsterMage() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MonsterMage::~MonsterMage() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ MonsterMage.h
@@ -0,0 +1,19 @@
+/*
+ * MonsterMage.h
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef MONSTERMAGE_H_
+#define MONSTERMAGE_H_
+
+#include "Mage.h"
+
+class MonsterMage: public Mage {
+public:
+	MonsterMage();
+	virtual ~MonsterMage();
+};
+
+#endif /* MONSTERMAGE_H_ */
