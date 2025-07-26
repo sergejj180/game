5167a1f92be67c2f7b50b40ac2aee17d6c710dec
sergejj180 <sergejj180@gmail.com>
2025-07-27 01:40:57 +0700

    date day 27 month 07 year 2025 time 01 hour 40 minute
    create class Warrior

---
+++ Warrior.cpp
@@ -0,0 +1,18 @@
+/*
+ * Warrior.cpp
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "Warrior.h"
+
+Warrior::Warrior() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Warrior::~Warrior() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Warrior.h
@@ -0,0 +1,17 @@
+/*
+ * Warrior.h
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef WARRIOR_H_
+#define WARRIOR_H_
+
+class Warrior {
+public:
+	Warrior();
+	virtual ~Warrior();
+};
+
+#endif /* WARRIOR_H_ */
