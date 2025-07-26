71d9cf030e6d62d7222d640cd63e56ee2afc585f
sergejj180 <sergejj180@gmail.com>
2025-07-27 01:36:53 +0700

    date day 27 month 07 year 2025 time 01 hour 36 minute
    create class Player

---
+++ Player.cpp
@@ -0,0 +1,18 @@
+/*
+ * Player.cpp
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "Player.h"
+
+Player::Player() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Player::~Player() {
+	// TODO Auto-generated destructor stub
+}
+
