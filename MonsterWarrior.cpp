f3f32767cf2381ea6b015e22e3cae9c4a1a4b73e
sergejj180 <sergejj180@gmail.com>
2025-07-27 01:56:42 +0700

    date day 27 month 07 year 2025 time 01 hour 56 minute
    create class MonsterWarrior

---
+++ MonsterWarrior.cpp
@@ -0,0 +1,18 @@
+/*
+ * MonsterWarrior.cpp
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "MonsterWarrior.h"
+
+MonsterWarrior::MonsterWarrior() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MonsterWarrior::~MonsterWarrior() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ MonsterWarrior.h
@@ -0,0 +1,19 @@
+/*
+ * MonsterWarrior.h
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef MONSTERWARRIOR_H_
+#define MONSTERWARRIOR_H_
+
+#include "Warrior.h"
+
+class MonsterWarrior: public Warrior {
+public:
+	MonsterWarrior();
+	virtual ~MonsterWarrior();
+};
+
+#endif /* MONSTERWARRIOR_H_ */
