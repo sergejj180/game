c9d523acbcf44af5385eccbc38b4da1d5b268cbe
sergejj180 <sergejj180@gmail.com>
2025-07-27 03:34:27 +0700

    date day 27 month 07 year 2025 time 03 hour 34 minute
    Add My fantasy to game project
    My fantasy is worked ! ! !

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+#include "Monster.h"
+
 int main(int argc, char **argv) {
 	std::cout << "Hello World" << std::endl;
+
+	Monster *objectMonster;
+
+	objectMonster = new Monster(1);
+	objectMonster = new Monster(2);
+	objectMonster = new Monster(3);
+
+	std::cout << "start project" << std::endl;
+
+	delete objectMonster;
+
+	objectMonster = nullptr;
 
 	return 0;
 }
