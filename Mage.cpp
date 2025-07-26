606b78cd4146b53ce4a6beaa4b9844a1b63d41c2
sergejj180 <sergejj180@gmail.com>
2025-07-27 01:48:06 +0700

    date day 27 month 07 year 2025 time 01 hour 47 minute
    create class Mage

---
+++ Mage.cpp
@@ -0,0 +1,18 @@
+/*
+ * Mage.cpp
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "Mage.h"
+
+Mage::Mage() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Mage::~Mage() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Mage.h
@@ -0,0 +1,17 @@
+/*
+ * Mage.h
+ *
+ *  Created on: 27 июл. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef MAGE_H_
+#define MAGE_H_
+
+class Mage {
+public:
+	Mage();
+	virtual ~Mage();
+};
+
+#endif /* MAGE_H_ */
