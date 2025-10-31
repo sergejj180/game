af88c20617934772043143d771aa4432951526cb
sergejj180 <sergejj180@sdsdsdsd>
2025-10-31 14:23:19 +0700

    date day 31 month 10 year 2025 time 14 hour 22 minute
    create class Node to game project

---
+++ Node.cpp
@@ -0,0 +1,9 @@
+/*
+ * Node.cpp
+ *
+ *  Created on: 31 окт. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "Node.h"
+
---
+++ Node.h
@@ -0,0 +1,14 @@
+/*
+ * Node.h
+ *
+ *  Created on: 31 окт. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef NODE_H_
+#define NODE_H_
+
+class Node {
+};
+
+#endif /* NODE_H_ */
