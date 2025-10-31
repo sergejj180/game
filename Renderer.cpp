6c646a5d469aa295f42f212c992ab01a6e3eabb1
sergejj180 <sergejj180@sdsdsdsd>
2025-10-31 14:09:19 +0700

    date day 31 month 10 year 2025 time 14 hour 08 minute
    create class Renderer to game project

---
+++ Renderer.cpp
@@ -0,0 +1,9 @@
+/*
+ * Renderer.cpp
+ *
+ *  Created on: 31 окт. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "Renderer.h"
+
---
+++ Renderer.h
@@ -0,0 +1,14 @@
+/*
+ * Renderer.h
+ *
+ *  Created on: 31 окт. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef RENDERER_H_
+#define RENDERER_H_
+
+class Renderer {
+};
+
+#endif /* RENDERER_H_ */
