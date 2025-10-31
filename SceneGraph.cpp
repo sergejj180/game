d69b063118c98d4258559ef7e9ea88ab4afa2b75
sergejj180 <sergejj180@sdsdsdsd>
2025-10-31 14:05:51 +0700

    date day 31 month 10 year 2025 time 14 hour 04 minute
    create class SceneGraph to game project

---
+++ SceneGraph.cpp
@@ -0,0 +1,9 @@
+/*
+ * SceneGraph.cpp
+ *
+ *  Created on: 31 окт. 2025 г.
+ *      Author: sergejj180
+ */
+
+#include "SceneGraph.h"
+
---
+++ SceneGraph.h
@@ -0,0 +1,14 @@
+/*
+ * SceneGraph.h
+ *
+ *  Created on: 31 окт. 2025 г.
+ *      Author: sergejj180
+ */
+
+#ifndef SCENEGRAPH_H_
+#define SCENEGRAPH_H_
+
+class SceneGraph {
+};
+
+#endif /* SCENEGRAPH_H_ */
